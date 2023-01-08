e2eb26ba67a5a627350ea941947f1fcc624a090d
Celso Palmeira Neto <neto@celsoneto.com.br>
2023-01-08 16:01:21 +0000

    feat: add convict to manage env vars

---
+++ packages/api/src/config.ts
@@ -0,0 +1,34 @@
+import convict from 'convict';
+
+export const config = convict({
+  database: {
+    host: {
+      default: 'localhost',
+      env: 'API_DB_HOST',
+      format: String,
+    },
+    port: {
+      default: 3306,
+      env: 'API_DB_PORT',
+      format: Number,
+    },
+    username: {
+      default: '',
+      env: 'API_DB_USERNAME',
+      format: String,
+    },
+    password: {
+      default: '',
+      env: 'API_DB_PASSWORD',
+      sensitive: true,
+      format: String,
+    },
+    database: {
+      default: 'finances',
+      env: 'API_DB_DATABASES',
+      format: String,
+    },
+  },
+});
+
+config.validate();
